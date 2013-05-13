df57d41e1c3697b2ff613f6ed4b864f1a5b59699
soundslikebliss <russianid1@gmail.com>
2013-05-14 00:54:51 +0200

    Added Devise

--- first_app/config/application.rb
+++ first_app/config/application.rb
@@ -58,5 +58,9 @@ module FirstApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-  end
+
+    # Required for Devise on Heroku
+    config.assets.initialize_on_precompile = false
 end
+end
+
